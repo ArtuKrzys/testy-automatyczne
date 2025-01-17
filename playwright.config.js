ab17221f9441455d58ef89bc6cdbb5edfb0eb0bb
ArtuKrzys <artur.krzysiek@protonmail.com>
2025-01-17 11:54:30 +0100

    junit reporter added

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,10 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }], // HTML dla lokalnego debugowania
+    ['junit', { outputFile: 'results.xml' }],       // JUnit dla CI
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
